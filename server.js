fa9f9cdb860511b53eed6b3c6087ae3d829d73d4
Louise Eriksson <epost.louise@gmail.com>
2020-06-05 17:37:21 +0200

    created the mongoose model 'Product' and added a few products to test. Also did a data.json file and added a few products there aswell to see wich way to go.

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
+import data from './data.json'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/final-project-backend'
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Product = mongoose.model('Product', {
+  title: String,
+  info: String,
+  price: Number
+})
+
+const seedDatabase = async () => {
+  await Product.deleteMany()
+  data.forEach((product) => new Product(product).save())
+
+  new Product({ title: 'Monstera Deliciosa', info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", price: 399 }).save()
+  new Product({ title: 'Philodendron Heartleaf', info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", price: 99 }).save()
+  new Product({ title: 'Zamioculcas Zamiifolia', info: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Why do we use it? It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", price: 199 }).save()
+}
+
+seedDatabase()
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -15,7 +34,34 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello hello world')
+})
+
+app.get('/products', (req, res) => {
+
+  const title = req.query.title
+
+  if (title) {
+    data = data.filter((item) => item.title)
+  }
+
+  const info = req.query.info
+
+  if (info) {
+    data = data.filter((item) => item.info)
+  }
+
+  const price = req.query.price
+
+  if (price) {
+    data = data.filter((item) => item.price)
+  }
+
+  if (data.length === 0) {
+    res.status(404).send('Not found, try again!')
+  } else {
+    res.json(data)
+  }
 })
 
 // Start the server
